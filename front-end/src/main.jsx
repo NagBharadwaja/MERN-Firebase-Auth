ed2436465c2c1a0fd1c4ac823e34bb1c01530741
Naga <nag.bharadwaja@gmail.com>
2025-04-06 02:24:55 -0400

    Remove credentials info on the client

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,15 +4,7 @@ import { initializeApp } from "firebase/app";
 
 import App from './App.jsx'
 import './index.css'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDjP3ulSOsVBd3zl1ouSZc4sE35qWFtjhM",
-  authDomain: "full-stack-react-1c177.firebaseapp.com",
-  projectId: "full-stack-react-1c177",
-  storageBucket: "full-stack-react-1c177.firebasestorage.app",
-  messagingSenderId: "140280139151",
-  appId: "1:140280139151:web:ea74618684aa1cae440ffa"
-};
+import { firebaseConfig } from '../credentials.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
